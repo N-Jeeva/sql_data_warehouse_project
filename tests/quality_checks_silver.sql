e4de634a548be508d91098a1c6dcb3807e7d1153
N-Jeeva <jeevanagarajan1414@gmail.com>
2025-10-08 22:45:16 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -18,3 +18,52 @@ Usage Notes:
 - Investigate and resolve any issues found during the checks.
 ==============================================================================
 */
+
+-- =============================================================================
+-- Checking 'silver.crm_cust_info'
+-- =============================================================================
+
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+        SELECT cst_id,
+        COUNT(*) FROM silver.crm_cust_info
+        GROUP BY cst_id
+        HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+
+-- =============================================================================
+-- Checking 'silver.silver.erp_cust_az12'
+-- =============================================================================
+-- Data Standardization & Consistency
+
+        SELECT DISTINCT gen
+        FROM silver.erp_cust_az12;
+
+-- Invalid date ranges and orders
+        SELECT * FROM silver.erp_cust_az12
+        WHERE bdate > CURRENT_DATE() OR bdate < '1925-01-01';
+-- =============================================================================
+-- Checking 'silver.erp_loc_a101'
+-- =============================================================================
+
+-- Data Standardization & Consistency
+        SELECT DISTINCT cntry
+        FROM silver.erp_loc_a101
+        ORDER BY cntry;
+
+-- ===============================================
+-- Checking 'silver.erp_px_cat_glv2'
+-- ===============================================
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+
+        SELECT * FROM silver.erp_px_cat_glv2
+        WHERE
+            cat != TRIM(cat)
+            OR subcat != TRIM(subcat)
+            OR maintenance != TRIM(maintenance);
+
+
+-- Data Standardization & Consistency
+        SELECT DISTINCT maintenance
+        FROM silver.erp_px_cat_glv2;
